d7adda00b1b5b68cec0d0412cbe02c4767f050ae
Joseph Tawil <jtawil2015@gmail.com>
2020-05-04 14:00:54 -0700

    reorganized my table

---
+++ README.md
@@ -0,0 +1,9 @@
+# Work Day Schedule
+
+* This is a Schedule Table where users can insert upcoming events into their calender.
+
+* In this project, I used a variety of tools to complete this project. To create the visual aspects of the table, I used Bootstrap's grid tools. For the styling, I had used CSS to differentiate the buttons on the screen.
+
+* For the functionality of the application, I used local storage to store the user's input into the user's local storage. Whenever the user refreshes the page, the data will still be present.
+
+* I am still working on the deletion of events from the local storage and the table.
--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 $(document).ready(function () {
     var div = document.querySelector(".container");
-    var date = moment().format('MMMM Do YYYY, h:mm:ss a');
-    $(div).prepend(date);
-    var button = document.querySelector("#button");
-    var text = document.querySelector("#row");
-    var toggleBackground = text.getAttribute("class");
+    var date = moment().format('MMMM Do YYYY');
+    var del = document.querySelector("#delete");
+    $(".jumbotron").append(`<h2>${date}</h2>`);
     var inputText = "";
-    var localStorage = window.localStorage;
-    $(button).on("click", function (e) {
-        e.preventDefault();
+    var button = document.querySelector(".saveBtn");
+
+
+    $("#button").on("click", function () {
         inputText = $("#text").val();
-        localStorage.setItem('Event', inputText);
-        if (toggleBackground === "col-10 alert alert-secondary") {
-            text.setAttribute("class", "col-10 alert alert-success");
-        }
+        localStorage.setItem("Event", inputText);
+        var data = localStorage.getItem("Event");
+        $("#area").append(`<h3 style="text-align:center">${inputText}</h3>`);
+
     });
+
+    if (localStorage != null) {
+        $("#area").append(`<h3 style="text-align:center">${localStorage.getItem("Event")}</h3>`);
+    }
+
+    function removeEvent() {
+        $("#delete").on("click", function () {
+            localStorage.removeItem("Event");
+            $("#area").html("");
+        });
+    };
 });
+
